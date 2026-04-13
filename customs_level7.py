e3f230935b3355c9a2ae7be879a10b08c340ed06
theserver88 <102000400+theserver88@users.noreply.github.com>
2026-04-13 17:11:03 +0200

    Add files via upload

---
+++ customs_level7.py
@@ -0,0 +1,17 @@
+#creando funcion, a partir de declararla con def
+
+
+
+def saludar():
+    print("Hola Juan como andas")
+
+
+def give_number(x):
+    if x == 3:
+        print("dritte")
+    else:
+        return x
+
+
+output = give_number(3)
+
